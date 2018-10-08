3716d4a3def6431d1d0b7db34d1f751d06f4f018
JOSE8 <luisj@discovery.co.za>
2018-10-08 16:18:35 +0200

    Backup

---
+++ Assets/LP_Framework/Scripts/OperatingSystem.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+namespace LPFramework {
+    public class OperatingSystem : MonoBehaviour {
+        #region Variables
+
+        [Header("Main Properties")]
+        public Screen startScreen;
+
+        [Header("System Events")]
+        public UnityEvent onSwitchedScreen = new UnityEvent();
+
+        [Header("Fader Properties")]
+        public Image fader;
+        public float fadeInDuration = 1f;
+        public float fadeOutDuration = 1f;
+
+        private Screen[] _screens = new Screen[0];
+        private Screen _previousScreen;
+        private Screen _currentScreen;
+
+        #endregion
+
+        #region Main
+
+        private void Start() {
+            _screens = GetComponentsInChildren<Screen>(true);
+            InitializeScreens();
+
+            if (startScreen) {
+                SwitchScreen(startScreen);
+            }
+
+            if (fader) {
+                fader.gameObject.SetActive(true);
+            }
+
+            FadeIn();
+        }
+
+        #endregion
+
+        #region Helper Methods
+
+        public void FadeIn() {
+            if (fader) {
+                fader.CrossFadeAlpha(0f, fadeInDuration, false);
+            }
+        }
+
+        public void FadeOut() {
+            if (fader) {
+                fader.CrossFadeAlpha(1f, fadeOutDuration, false);
+            }
+        }
+
+        public void SwitchScreen(Screen screen) {
+            if (screen) {
+                if (_currentScreen) {
+                    _currentScreen.CloseScreen();
+                    _previousScreen = _currentScreen;
+                }
+
+                _currentScreen = screen;
+                _currentScreen.gameObject.SetActive(true);
+                _currentScreen.StartScreen();
+
+                if (onSwitchedScreen != null) {
+                    onSwitchedScreen.Invoke();
+                }
+            }
+        }
+
+        public void GotoPreviousScreen() {
+            if (_previousScreen) {
+                SwitchScreen(_previousScreen);
+            }
+        }
+
+        public void LoadScene(int sceneIndex) {
+            StartCoroutine(WaitToLoadScene(sceneIndex));
+        }
+
+        private IEnumerator WaitToLoadScene(int sceneIndex) {
+            yield return null;
+        }
+
+        private void InitializeScreens() {
+            foreach (Screen screen in _screens) {
+                screen.gameObject.SetActive(true);
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ Assets/LP_Framework/Scripts/Screens/Screen.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace LPFramework {
+    [RequireComponent(typeof(Animator))]
+    [RequireComponent(typeof(CanvasGroup))]
+    public class Screen : MonoBehaviour {
+        #region Variables
+
+        [Header("Main Properties")]
+        public Selectable startSelectable;
+
+        [Header("Screen Events")]
+        public UnityEvent onScreenStart = new UnityEvent();
+        public UnityEvent onScreenClose = new UnityEvent();
+
+        private Animator _animator;
+
+        #endregion
+
+        #region Main
+
+        private void Start() {
+            _animator = GetComponent<Animator>();
+
+            if (startSelectable) {
+                EventSystem.current.SetSelectedGameObject(startSelectable.gameObject);
+            }
+        }
+
+        #endregion
+
+        #region Helper Methods
+
+        public virtual void StartScreen() {
+            if (onScreenStart != null) {
+                onScreenStart.Invoke();
+            }
+
+            HandleAnimator("show");
+        }
+        
+        public virtual void CloseScreen() {
+            if (onScreenClose != null) {
+                onScreenClose.Invoke();
+            }
+            
+            HandleAnimator("hide");
+        }
+
+        private void HandleAnimator(string trigger) {
+            if (_animator) {
+                _animator.SetTrigger(trigger);
+            }
+        }
+        #endregion
+    }
+}
